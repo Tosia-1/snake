d2f2f8ccf954c2bc9bc36e7ef9ee59a8e2c46188
Tosia <192628483+Tosia-1@users.noreply.github.com>
2026-04-09 23:08:09 +0300

    moving rectangle

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 using namespace std;
 
 #include "raylib.h"
@@ -6,12 +7,25 @@ using namespace std;
 int main()
 {
     InitWindow(800, 450, "raylib example - basic window");
-
+    int posX = 200;
+    int posY = 200;
+    SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+        if (IsKeyPressed(KEY_RIGHT)) {
+            posX += 2;
+            cout << posX << endl;
+        }
+        if (IsKeyPressed(KEY_LEFT)) {
+            posX -= 2;
+        }
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        // Rectangle
+        DrawFPS(20, 20);
+        DrawRectangle(posX, posY, 120, 60, RED);
         EndDrawing();
     }
 
