55c807a83c3b6b3ac8d12722ade1cdfbab91cb57
Tosia <192628483+Tosia-1@users.noreply.github.com>
2026-04-15 13:08:04 +0300

    move block in directions

--- main.cpp
+++ main.cpp
@@ -1,31 +1,72 @@
 #include <iostream>
-#include <unistd.h>
-using namespace std;
-
+#include <cmath>
+#include <deque>
 #include "raylib.h"
 
-int main()
-{
-    InitWindow(800, 450, "raylib example - basic window");
-    int posX = 200;
-    int posY = 200;
-    SetTargetFPS(60);
+using namespace std;
+
+
+enum Dir {N = 1, E, S, W};
+
+class Block {
+public:
+    double x;
+    double y;
+    double length;
+
+    void draw(int width) {
+        DrawRectangle(round(x), round(y), round(length), width, DARKGREEN);
+    }
+};
+
+int main() {
+    InitWindow(800, 450, "snake");
+    SetTargetFPS(30);
+
+    int width = 20;
+    int speed = 70; // pixels per second
+
+
+    deque<Block> snake;
+    snake.push_back({200, 200, 100});
+    Dir direction{Dir::E};
+
     while (!WindowShouldClose())
     {
         if (IsKeyPressed(KEY_RIGHT)) {
-            posX += 2;
-            cout << posX << endl;
+            direction = Dir::E;
+        }
+        if (IsKeyPressed(KEY_UP)) {
+            direction = Dir::N;
+        }
+        if (IsKeyPressed(KEY_DOWN)) {
+            direction = Dir::S;
         }
         if (IsKeyPressed(KEY_LEFT)) {
-            posX -= 2;
+            direction = Dir::W;
         }
+
+        double frameTime = GetFrameTime();
+        if (direction == Dir::E) {
+            snake.at(0).x += speed * frameTime;
+        }
+        else if (direction == Dir::S) {
+            snake.at(0).y += speed * frameTime;
+        }
+        else if (direction == Dir::N) {
+            snake.at(0).y -= speed * frameTime;
+        }
+        else if (direction == Dir::W) {
+            snake.at(0).x -= speed * frameTime;
+        }
+        //cout << frame_time << endl;
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        // Rectangle
         DrawFPS(20, 20);
-        DrawRectangle(posX, posY, 120, 60, RED);
+        for (auto bl : snake) {
+            bl.draw(width);
+        }
         EndDrawing();
     }
 
